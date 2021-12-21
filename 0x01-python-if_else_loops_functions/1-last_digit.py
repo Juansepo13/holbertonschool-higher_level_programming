925f61a4406206947ec6ab33a08698a428977685
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2021-12-21 18:04:38 -0500

    Update 1-last_digit.py
    new code

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+# abs is to calculate the absolute of a number
 if number < 0:
-        new = number * (-1)
-        last = new % 10
-        last *= (-1)
+    lastDigit = abs(number) % 10 * -1
 else:
-        last = number % 10
-        if last > 5:
-                r = ("and is greater than 5")
-        elif last == 0:
-                r = ("and is 0")
-        elif last != 0 and last < 6:
-                r = ("and is less than 6 and not 0")
-                print("Last digit of {} is {} {}".format(number, last, r))
+    lastDigit = number % 10
+
+# The "end" key of print function will set the string that needs
+#  to be appended when printing is done.
+# ends=""allow me to append and print in the conitionals, instead of new line.
+
+print("Last digit of {0:d} is {1:d} ".format(number, lastDigit), end="")
+
+if lastDigit > 5:
+    print("and is greater than 5")
+elif lastDigit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
