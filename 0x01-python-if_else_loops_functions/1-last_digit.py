a0841040dea0ea3ff31c2d9d2f0c1bd2055a619c
Juansepo13 <3895@holbertonschool.com>
2021-12-21 15:35:10 -0500

    add fixed code check 2 4 and 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str = "Last digit of "
-last = number % 10
-if number < 0:
-        last -= 10
-        if last > 5:
-                print(str+"{:d} is {:d} and is greater than 5".format(number, last))
-        elif last == 0:
-                print(str+"{:d} is {:d} and is 0".format(number, last))
+str = "and is less than 6 and not 0"
+str1 = "and is greater than 5"
+if number >= 0:
+        last_digit = number % 10
+else:
+        last_digit = number % -10
+        if last_digit == 0:
+                print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+        elif last_digit < 6 and last_digit != 0:
+                print("Last digit of {:d} is {:d}".format(number, last_digit), str)
         else:
-                print(str+"{:d} is {:d} and is less than 6 and not 0".format(number, last))
+                print("Last digit of {:d} is {:d}".format(number, last_digit), str1)
