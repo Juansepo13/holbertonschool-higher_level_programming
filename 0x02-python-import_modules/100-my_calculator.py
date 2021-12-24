0caa639e84a667b2c850a7e598d3407a8096a004
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2021-12-24 09:53:01 -0500

    Update 100-my_calculator.py
    correct again file, fixed line 18

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    value = 0
-    from calculator_1 import add, div, sub, mul
-    import sys
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = ""
-    operator = sys.argv[2]
-    for w in sys.argv:
-        value = value + 1
-    if (value - 1) < 3:
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    var = len(argv) - 1
+    if var != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if operator == '+':
-        print('{} + {} = {}'.format(a, b, add(a, b)))
-    elif operator == '/':
-        print('{} / {} = {}'.format(a, b, div(a, b)))
-    elif operator == '-':
-        print('{} - {} = {}'.format(a, b, sub(a, b)))
-    elif operator == '*':
-        print('{} * {} = {}'.format(a, b, mul(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    a = int(argv[1])
+    op = str(argv[2])
+    b = int(argv[3])
+    if op not in ['+', '-', '*', '/']:
+        msg = "Unknown operator. Available operators: +, -, * and /"
+        print(msg, end='\n')
         exit(1)
+    else:
+        eq = {'+': add(a, b), '*': mul(a, b), '-': sub(a, b), '/': div(a, b)}
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, eq.get(op)), end='\n')
