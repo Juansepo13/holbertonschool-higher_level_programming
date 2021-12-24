e68b30f026aa36ccdd35236f541ebf6fc3b7e723
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2021-12-24 09:38:15 -0500

    Update 100-my_calculator.py
    fixed my owned calculator problems identation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    value = 0
+    from calculator_1 import add, div, sub, mul
+    import sys
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = ""
+    operator = sys.argv[2]
+    for w in sys.argv:
+        value = value + 1
+    if (value - 1) < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if operator == '+':
+        print('{} + {} = {}'.format(a, b, add(a, b)))
+    elif operator == '/':
+        print('{} / {} = {}'.format(a, b, div(a, b)))
+    elif operator == '-':
+        print('{} - {} = {}'.format(a, b, sub(a, b)))
+    elif operator == '*':
+        print('{} * {} = {}'.format(a, b, mul(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
