052dcbe3578d5312406f1fa19a4e9b414eb6d99d
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2021-12-24 10:24:05 -0500

    Update 1-calculation.py
    Edit code with identation python"

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-    
+    # add
+    print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add(a, b))))
+    # sub
+    print("{:s} - {:s} = {:s}".format(str(a), str(b), str(sub(a, b))))
+    # mul
+    print("{:s} * {:s} = {:s}".format(str(a), str(b), str(mul(a, b))))
+    # div
+    print("{:s} / {:s} = {:s}".format(str(a), str(b), str(div(a, b))))
