3c72b53cf24ea0826ec7e10ba7bd9cb486c02aad
Juansepo13 <3895@holbertonschool.com>
2022-01-17 18:18:02 -0500

    add all files

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    from sys import stderr
+    try:
+        print ("{:d}".format(value))
+        return True
+    except Exception as error:
+        print("Exception: {}".format(error), file=stderr)
+        return False
