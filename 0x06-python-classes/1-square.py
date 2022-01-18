1ac4eef389c017a87055cc8900bf5b050c341046
Juansepo13 <3895@holbertonschool.com>
2022-01-18 17:00:09 -0500

    add file 1square.py task 1 square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create new class Square"""
+
+
+class Square():
+        """Class Square empty"""
+        def __init__(self, size):
+            """Instantiation the square with size."""
+            self.__size = size
