f03eafe08b6d7ea80a899255d4b57b679f1485bf
Juansepo13 <3895@holbertonschool.com>
2022-01-18 17:51:27 -0500

    add file 4square.py access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that creates an empty square."""
+
+
+class Square:
+    """Empty square."""
+    def __init__(self, size = 0):
+        """Initialize square with size."""
+        self.__size = size
+
+        def area(self):
+            """returns the current square area"""
+            return self.__size * self.__size
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
