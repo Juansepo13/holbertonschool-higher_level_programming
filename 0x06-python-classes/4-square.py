3929428026a003adde2ba116736f5a1f7c3f87e6
Juansepo13 <3895@holbertonschool.com>
2022-01-18 18:36:34 -0500

    add file 4 after correction more comments

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,7 +2,7 @@
 """Module that creates an empty square."""
 
 
-class Square:
+class Square():
     """Empty square."""
     def __init__(self, size=0):
         """Initialize square with size."""
@@ -14,10 +14,12 @@ class Square:
 
         @property
         def size(self):
+            """rescue the size of the square"""
             return self.__size
 
         @size.setter
         def size(self, value):
+            """Set the size of the square!"""
             if type(size) is not int:
                 raise TypeError("size must be an integer")
             if size < 0:
