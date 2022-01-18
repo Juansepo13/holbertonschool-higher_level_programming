4bf134671650b5736f8492c2d7fdacd49ee580c6
Juansepo13 <3895@holbertonschool.com>
2022-01-18 18:40:21 -0500

    add changues in file4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,8 +20,8 @@ class Square():
         @size.setter
         def size(self, value):
             """Set the size of the square!"""
-            if type(size) is not int:
+            if not isinstance(new_size, int):
                 raise TypeError("size must be an integer")
-            if size < 0:
+            elif new_size < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = value
+            self.__size = new_size
