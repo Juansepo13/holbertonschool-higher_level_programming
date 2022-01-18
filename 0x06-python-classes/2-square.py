c1547c2631ccae648251a609250d82bcb60cd601
Juansepo13 <3895@holbertonschool.com>
2022-01-18 17:29:11 -0500

    add file 2square task2 validation square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that creates an empty square."""
+
+
+class Square:
+    """Empty square."""
+    def __init__(self, size = 0):
+        """Initialize square with size."""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
