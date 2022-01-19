75ee4f77a15a465f59207703fafdcd732dc6abe3
Juansepo13 <3895@holbertonschool.com>
2022-01-18 21:54:20 -0500

    add file 4-square delete () in the end 5 line

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,26 +2,26 @@
 """Create new class Square"""
 
 
-class Square():
+class Square:
     """Class Square empty"""
     def __init__(self, size=0):
         """Initialize square with size."""
         self.__size = size
 
-        def area(self):
-            """returns the current square area"""
-            return self.__size * self.__size
+    def area(self):
+        """Returns current square area."""
+        return self.__size * self.__size
 
-        @property
-        def size(self):
-            """rescue the size of the square"""
-            return self.__size
+    @property
+    def size(self):
+        """Rescue the square's size"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Set the size of the square!"""
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """Set the square's size!"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
