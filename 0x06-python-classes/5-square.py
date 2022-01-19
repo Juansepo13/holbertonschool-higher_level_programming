9469218166936d962e1484f2c5ed3941a985d4b4
Juansepo13 <3895@holbertonschool.com>
2022-01-18 21:48:28 -0500

    add all files

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Create new class Square"""
+"""Module that creates an empty square."""
 
 
-class Square():
-    """Class Square empty"""
+class Square:
+    """Empty square."""
     def __init__(self, size=0):
         """Initialize square with size."""
         self.__size = size
@@ -20,8 +20,16 @@ class Square():
         @size.setter
         def size(self, value):
             """Set the size of the square!"""
-            if type(value) is not int:
+            if type(size) is not int:
                 raise TypeError("size must be an integer")
-            if value < 0:
+            if size < 0:
                 raise ValueError("size must be >= 0")
             self.__size = value
+
+        def my_print(self):
+            """prints in stdout the square with the character #:"""
+            """if size is equal to 0, print an empty line"""
+            for i in range(self.__size):
+                print("#" * self.__size))
+            if self.__size is 0:
+                print()
