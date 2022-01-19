1a8b0293dc777f0b2d2e89ffca3642639932728e
Juansepo13 <3895@holbertonschool.com>
2022-01-18 22:19:01 -0500

    add file 5 printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-"""Module that creates an empty square."""
+"""Create new class Square"""
 
 
 class Square:
-    """Empty square."""
+    """Class Square empty"""
     def __init__(self, size=0):
         """Initialize square with size."""
         self.__size = size
 
-        def area(self):
-            """returns the current square area"""
-            return self.__size * self.__size
+    def area(self):
+        """Returns current square area."""
+        return self.__size * self.__size
 
-        @property
-        def size(self):
-            """rescue the size of the square"""
-            return self.__size
+    @property
+    def size(self):
+        """Rescue the square's size"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Set the size of the square!"""
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """Set the square's size!"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def my_print(self):
-            """prints in stdout the square with the character #:"""
-            """if size is equal to 0, print an empty line"""
-            for i in range(self.__size):
-                print("#" * self.__size))
-            if self.__size is 0:
-                print()
+    def my_print(self):
+        """Prints in stdout the square with the character #:"""
+        """if size is equal to 0, print an empty line"""
+        for i in range(self.__size):
+            print("#" * self.__size)
+        if self.__size is 0:
+            print()
