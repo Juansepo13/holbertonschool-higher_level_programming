4c65b575a596fba41d63aaa73a3febba38da2ab9
Juansepo13 <3895@holbertonschool.com>
2022-01-31 15:38:49 -0500

    add file 1 my list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that define a class.
+"""
+
+
+class MyList(list):
+    """
+    Class MyList
+    """
+    def print_sorted(self):
+        """
+        Function that prints a list, sorted (asceding sort).
+        """
+        print(sorted(self))
