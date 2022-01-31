24b2824ae6d25bb3315d295ba5e9e57483183b44
Juansepo13 <3895@holbertonschool.com>
2022-01-31 16:23:29 -0500

    add 7base geometys

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that define an empty class."""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+    def area(self):
+        raise Exception("are() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates value."""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater that 0")
