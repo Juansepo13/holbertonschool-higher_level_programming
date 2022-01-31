32c21fc5c45eaf1e232a9d70d6dc15c10fc6c1f6
Juansepo13 <3895@holbertonschool.com>
2022-01-31 16:23:16 -0500

    add 6 main py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that define an empty class."""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
