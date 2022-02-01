3508b95912b045dbf82667d4be4ac74fcbf1c126
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-01-31 22:02:43 -0500

    Update 7-base_geometry.py
    new identation with python iddle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,10 +6,10 @@ Module that define an empty class.
 
 class BaseGeometry:
     """
-    class BaseGeometry.
+    Class BaseGeometry.
     """
     def area(self):
-        raise Exception("are() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
@@ -18,4 +18,4 @@ class BaseGeometry:
         if type(value) is not int:
             raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError(name + " must be greater that 0")
+            raise ValueError(name + " must be greater than 0")
