083c4717cc3faa61d5edd748bcb7fab7c03b4945
Juansepo13 <3895@holbertonschool.com>
2022-01-31 20:14:31 -0500

    add file c changue comentarios

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,14 @@
 #!/bin/user/python3
-"""Write a function that returns the list of
-   available attributes and methods of an object"""
+"""
+lookup: Function thar returns the list.
+    Args:
+    obj: object.
+"""
+
+
 def lookup(obj):
-    """Prototype: def lookup(obj)
-       Returns a list object
-       You are not allowed to import any module"""
+    """lookup
+        Args:
+        obj: Object.
+    """
     return dir(obj)
