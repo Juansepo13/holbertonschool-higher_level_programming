0e404606363ad66437430c5f095a02e920dec8c8
Juansepo13 <3895@holbertonschool.com>
2022-01-31 22:09:04 -0500

    add new spaces and comentaries in line 10

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """Module that import a class."""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """class square"""
     def __init__(self, size):
+        """Initalize method"""
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
