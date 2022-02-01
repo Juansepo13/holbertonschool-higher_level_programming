c69416e85d25c24d5dccff72660a1adfa14d6cfa
Juansepo13 <3895@holbertonschool.com>
2022-01-31 22:07:39 -0500

    add new spaces in comentaries

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Contains a subclass of rectangle"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Rectangle"""
-
     def __init__(self, width, height):
         """Initalizaction the class"""
         self.__width = width
