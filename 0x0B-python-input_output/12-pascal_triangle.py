387463e4a2e915c46c5de9832f2c6240e76343f8
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-02-01 23:54:43 -0500

    Update 12-pascal_triangle.py
    add all files

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
     for i in range(2, n):
         row = [1]
         for j in range(1, i):
-                row.append(triangle[i-1][j-1] + triangle[i-1][j])
-            row.append(1)
-            triangle.append(row)
-        return triangle
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
