757b9909f4e55519eb31bfcd5187b37a0047db3b
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-02-07 14:25:23 -0500

    Update rectangle.py
    new identation with python idle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,124 +1,142 @@
 #!/usr/bin/python3
-"""Defining Rectangle Class."""
-
+"""Reactangle class that inherits of Base"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle Class inherits from Base."""
+    """Class new that inherits of Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes a Rectangle instance.
+        """class constructor, instance attributes
+        call super class, with id, call with __init__ ,  or class parent"""
 
-        Args:
-        - __width: width
-        -__height: height
-        -__x: position
-        -__y: position
-        -id: id
-        """
-
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
-        """Retrieves the width attribute"""
-
+        """getter and setter for each instance attributes"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the widht."""
+        """validations"""
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
     def height(self):
-        """Get the height."""
+        """property of height"""
+
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height."""
+        """setter of height"""
+
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         if value <= 0:
-            raise ValueError("height must be > 0")
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
     def x(self):
-        """Get x."""
+        """property x"""
+
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Set x."""
+        """setter x"""
+
         if type(value) is not int:
-            raise TypeError("x must be an integer")
+            raise TypeError('x must be an integer')
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
     def y(self):
-        """Get y."""
+        """property y"""
+
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Set y."""
+        """setter y"""
+
         if type(value) is not int:
-            raise TypeError("y must be an integer")
+            raise TypeError('y must be an integer')
         if value < 0:
-            raise ValueError("y must be >= 0")
+            raise ValueError('y must be >= 0')
         self.__y = value
 
     def area(self):
-        """Returns area value of Rectangle instance."""
-        return self.width * self.height
+        """update class Rectangle, add public method, return area value"""
+
+        return self.__width * self.__height
 
     def display(self):
-        """Prints in stdout Rectangle instance with character #."""
-        for i in range(self.__y):
-            print()
-        for i in range(self.__height):
-            print(" " * self.__x, end="")
-            print("#" * self.__width)
+        """verify y for print \n and iter and height print ' '
+        and  funally print #"""
+
+        print('\n' * self.__y, end='')
+        for iter_h in range(self.__height):
+            print(' ' * self.__x,  end='')
+            print("".join(["#" for i in range(self.__width)]))
+
+        """update class Rectangle, add public method,
+        print # for width and height"""
+        if self.__width == 0 or self.__height == 0:
+            print("")
 
     def __str__(self):
-        """Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height)
+        """Overriding method"""
+
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
-        """ public method def update that assigns an
-        argument to each attribute
-        -**kwargs must be skipped if *args exists and is not empty
-        -Each key in this dictionary represents an attribute to the instance
-        """
-        args_list = ["id", "width", "height", "x", "y"]
-        if args and args[0] is not None:
-            if len(args) > len(args_list):
-                max_len = len(args_list)
-            else:
-                max_len = len(args)
-            for i in range(max_len):
-                setattr(self, args_list[i], args[i])
-        elif kwargs is not None:
-            for key in kwargs:
-                if hasattr(self, key) is True:
-                    setattr(self, key, kwargs[key])
+        """method that assings ags each atri... and update  args and kwargs"""
+
+        if len(args):
+            for iter_args, arg in enumerate(args):
+                if iter_args == 0:
+                    self.id = arg
+                elif iter_args == 1:
+                    self.__width = arg
+                elif iter_args == 2:
+                    self.__height = arg
+                elif iter_args == 3:
+                    self.__x = arg
+                elif iter_args == 4:
+                    self.__y = arg
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.__width = value
+                elif key == "height":
+                    self.__height = value
+                elif key == "x":
+                    self.__x = value
+                elif key == "y":
+                    self.__y = value
 
     def to_dictionary(self):
-         """dictionary representation of a Square"""
-         key_list = ["id", "width", "height", "x", "y"]
-         value_list = [self.id, self.width, self.height, self.x, self.y]
-         return dict(zip(key_list, value_list))
+        """update class that returns the
+        dictionary representation of a Rectangle"""
+
+        vars = ['x', 'y', 'id', 'height', 'width']
+        dict = {}
+        for i in range(len(vars)):
+            dict.update({vars[i]: getattr(self, vars[i])})
+        return dict
