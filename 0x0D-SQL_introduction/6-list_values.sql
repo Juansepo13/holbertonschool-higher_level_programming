69ec1e4b9793afda1ae650dbfdb941fd7da54ffd
Juansepo13 <3895@holbertonschool.com>
2022-03-14 15:15:42 -0500

    add file 6 list all in table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Lists all rows of first_table from the database hbtn_0c_0 in a MySQL server.
+-- SELECT used to retrieve rows from one/more tables. Include UNION statements and subqueries.
+SELECT * FROM first_table;
