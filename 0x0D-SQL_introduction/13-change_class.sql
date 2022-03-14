ff4661d3b9a1c58fbe6fe7cf9c46561b6df0325d
Juansepo13 <3895@holbertonschool.com>
2022-03-14 15:57:06 -0500

    add all files

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Removes all records with a score <= 5 in the table second_table.
+-- DELETE statement deletes rows from tbl_name and returns the number of deleted rows.
+DELETE FROM second_table WHERE score <=5;
