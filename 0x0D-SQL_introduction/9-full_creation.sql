d6a3f19299c3f941dad52176fd43654bde96430c
Juansepo13 <3895@holbertonschool.com>
2022-03-14 15:48:19 -0500

    add file 9 full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- Creates a table second_table in the database hbtn_0c_0 in a MySQL server and add multiples rows.
+-- Syntax: CREATE [TEMPORARY] TABLE [IF NOT EXISTS] tbl_name.
+-- INSERT [INTO] tbl_name. Inserts new rows into an existing table.
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
