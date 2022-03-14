4d578d718e1ec1684e981d32811acbcb046a00d6
Juansepo13 <3895@holbertonschool.com>
2022-03-14 15:42:51 -0500

    add file first add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Inserts a new row in the table first_table.
+-- Syntax: INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE][INTO] tbl_name
+INSERT INTO first_table (id, name) VALUES(89, 'Best School');
