81e9074302af6928a801cd27c6d987cd7113b3da
Juansepo13 <3895@holbertonschool.com>
2022-03-14 15:49:35 -0500

    add file 10 list by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records of the table second_table of the database.
+-- SELECT is used to retrieve rows selected from one or more tables.
+SELECT score, name FROM second_table ORDER BY score DESC, name;
