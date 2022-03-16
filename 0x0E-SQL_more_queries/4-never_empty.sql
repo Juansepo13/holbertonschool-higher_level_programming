01c27f0d139563dbdeb94514cb8e50a15772a640
Juansepo13 <3895@holbertonschool.com>
2022-03-16 17:38:54 -0500

    add all file }4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- Creates the table id_not_null on a MySQL server.
+-- CREATE TABLE statement creates a table with the given name.
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
