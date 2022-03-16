10c91adbb72823cdc8cafa3eb197639109d7e8b0
Juansepo13 <3895@holbertonschool.com>
2022-03-16 17:26:31 -0500

    add file 0 privileges sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges of MySQL users user_0d_1 and user_0d_2 on a server.
+-- Displays privileges/roles assigned to a MySQL account/role.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
