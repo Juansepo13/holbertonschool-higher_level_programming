70cabed61a0cf83a37400710424385759206a40f
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-04 15:34:25 -0500

    add all files

---
+++ 0x12-javascript-warm_up/prueba.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const x = 1;
+const y = -1;
+
+console.log(+x);
+console.log(+y);
+console.log(+'');
+console.log(+true);
+console.log(+false);
+console.log(+'hello');
+console.log(+'hello'+'world');
