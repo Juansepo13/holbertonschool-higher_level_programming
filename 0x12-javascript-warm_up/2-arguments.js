e52bc3a4a92c7ae5434dcd5033deb12b3829f23a
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-04 16:47:56 -0500

    add file 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
 const args = process.argv;
-
-if (args.length > 3) {
-    console.log('Arguments found');
-} else if (args.length === 3) {
-    console.log('Argument found');
+if (args[2]) {
+    if (args[3]) {
+	console.log('Arguments found');
+    } else {
+	console.log('Argument found');
+    }
 } else {
     console.log('No argument');
 }
