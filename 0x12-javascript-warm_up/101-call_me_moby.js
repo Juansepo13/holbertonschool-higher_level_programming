3f4f6359f510040a03f4ea78eff7c5414855a407
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-04-05 19:39:07 -0500

    Update 101-call_me_moby.js
    new identatio with Visual Studio code

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const callMeMoby = require('./101-call_me_moby').callMeMoby;
-callMeMoby(3, function () {
-    console.log('C is fun');
-});
+exports.callMeMoby = function (x, theFunction) {
+    for (let i = 0; i < x; i++) {
+        theFunction();
+    }
+};
