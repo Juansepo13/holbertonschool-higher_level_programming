9bbb8f0c7cde143e0aca4ae95da317593bb61538
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-07 12:27:31 -0500

    add file Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+    let count = 0;
+    for (const index in list) {
+      if (list[index] === searchElement) {
+        count++;
+      }
+    }
+    return count;
+  };
+  
