f15119231c1d16b4c8ff077330a299be08763e48
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-07 15:03:24 -0500

    delete spaces in line 3 4 5 6 7 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const repList = [];
-    for (let i = 0; i < list.length; i++) {
-      repList.unshift(list[i]);
-    }
-    return repList;
-  };
+  const repList = [];
+  for (let i = 0; i < list.length; i++) {
+    repList.unshift(list[i]);
+  }
+  return repList;
+};
