8063bd6c8b9b4d7b2aa6c465e51fb7dd5343b904
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-07 12:33:31 -0500

    add file reverse

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    const repList = [];
+    for (let i = 0; i < list.length; i++) {
+      repList.unshift(list[i]);
+    }
+    return repList;
+  };
