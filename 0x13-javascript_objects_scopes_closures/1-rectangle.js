711ac9f512d74fd358528f1052442474a200f731
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-07 11:30:24 -0500

    add all files

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-<<<<<<< HEAD
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-    }
-  };
-=======
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 };
->>>>>>> 9af3e1bb5182c78c64235cf8aa95b7598af2e8d0
