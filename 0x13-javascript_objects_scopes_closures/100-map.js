0bb71e20aea99822fb03758b0277e837b139e47f
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-04-08 18:40:49 -0500

    Update 100-map.js
    New identation delete space

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const arr = list.map((x, i) => x * i);
+const repList = list.map((curr, index) => {
+  return (curr * index);
+});
 console.log(list);
-console.log(arr);
+console.log(repList);
