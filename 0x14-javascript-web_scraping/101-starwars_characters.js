c076c34dfe393e885a68a7f242498b95c2ba8514
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-12 11:15:16 -0500

    add file 101 right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie
+
+const request = require('request');
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (error, response, body) => {
+    if (error) throw error;
+    else {
+      send(JSON.parse(body).characters, 0);
+    }
+  }
+);
+
+function send (person, idx) {
+  if (idx >= person.length) {
+    return;
+  }
+  request(person[idx], (error, response, body) => {
+    if (error) throw error;
+    else {
+      console.log(JSON.parse(body).name);
+      return send(person, ++idx);
+    }
+  });
+}
