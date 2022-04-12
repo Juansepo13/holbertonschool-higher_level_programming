0a6bab8e55e218afa80a5c9d86317786b3ee8579
Juansepo13 <juancho_s1314@hotmail.com>
2022-04-12 10:50:36 -0500

    add new spaces and 2 lines

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,23 @@
 #!/usr/bin/node
-
+const args = process.argv;
+const reqURL = args[2];
 const request = require('request');
-const find = '/18/';
-request(process.argv[2], function (error, response, body) {
-  if (error) throw new Error(error);
-  let num = 0;
-  for (const film of JSON.parse(body).results) {
-    for (const character of film.characters) {
-      num += (character.includes(find) ? 1 : 0);
+request(reqURL, function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const jso = JSON.parse(body);
+    const results = jso.results;
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      const chars = (results[i].characters);
+      for (let j = 0; j < chars.length; j++) {
+        const check18 = chars[j].endsWith('18/');
+        if (check18) {
+          count++;
+        }
+      }
     }
+    console.log(count);
   }
-  console.log(num);
 });
