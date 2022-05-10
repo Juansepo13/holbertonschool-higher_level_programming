98d5c7a8135acdd529746022843e2831dfe5d9ab
Juansepo13 <juancho_s1314@hotmail.com>
2022-05-10 17:16:58 -0500

    add changues in file 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N (upper N)
-    from the database hbtn_0e_0_usa
 """
-if __name__ == "__main__":
+script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
+"""
 
-    import sys
-    import MySQLdb
-    
-    mydb = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           password=sys.argv[2], database=sys.argv[3])
-    mycursor = mydb.cursor()
-    q = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
-    mycursor.execute(q)
-    myresult = mycursor.fetchall()
-    for row in myresult:
-        print(row)
-    mycursor.close()
-    mydb.close()
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    sql = """SELECT * FROM states
+          WHERE name LIKE 'N%'
+          ORDER BY states.id ASC"""
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if 'N' == row[1][0]:
+            print(row)
+    cur.close()
+    db.close()
