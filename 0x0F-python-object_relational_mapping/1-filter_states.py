38d54e4dd0a06622777c3d1cafbbbb856753452f
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-05-10 16:38:57 -0500

    edit file correct fixxing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,10 +9,11 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', 
-                        port=3306, user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     _cursor = db.cursor()
     _cursor.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%' \
