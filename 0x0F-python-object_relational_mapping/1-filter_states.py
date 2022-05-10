4a55fc1827b706236beda614c6ab35de3c0dd930
Juan Posada <91068882+Juansepo13@users.noreply.github.com>
2022-05-10 16:19:17 -0500

    new fixxing code in 10 lines task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""Filter states"""
+'''list all states from the database hbtn_003_0usa'''
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    '''main function'''
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE states.name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
