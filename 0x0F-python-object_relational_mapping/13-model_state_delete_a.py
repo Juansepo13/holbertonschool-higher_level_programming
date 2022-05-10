4693abc07ecc29fdb2a603756afdfc0ebacd034d
Juansepo13 <juancho_s1314@hotmail.com>
2022-05-10 18:30:35 -0500

    add fixiing code

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Delete states"""
-
+"""Start link class to table in database
+"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
-    session.commit()
+    Base.metadata.create_all(engine)
+    sesionCRUD = Session(engine)
+    for state in sesionCRUD.query(State).filter(State.name.like('%a%')).all():
+        sesionCRUD.delete(state)
+    sesionCRUD.commit()
+    sesionCRUD.close()
