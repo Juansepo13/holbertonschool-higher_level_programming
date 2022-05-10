97419ff13535109ebd23ebbd595b448c456da9c1
Juansepo13 <juancho_s1314@hotmail.com>
2022-05-10 16:46:10 -0500

    changue a past code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
-import MySQLdb
+""" lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
 
-
-def connectDB():
-    """
-    Connect to database and quering
-    """
-    try:
-        db_connection = MySQLdb.connect(host="localhost", port=3306,
-                                        user=argv[1], password=argv[2],
-                                        db=argv[3], charset="utf8")
-    except Exception:
-        print("Can't connect to database")
-        return 0
-    cur = db_connection.cursor()
-    sql = "SELECT id, name FROM states ORDER BY id ASC;"
-    cur.execute(sql)
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if 'N' == row[1][0]:
-            print(row)
-    cur.close()
-    db_connection.close()
-
-
-connectDB()
+    import sys
+    import MySQLdb
+    
+    mydb = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           password=sys.argv[2], database=sys.argv[3])
+    mycursor = mydb.cursor()
+    q = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    mycursor.execute(q)
+    myresult = mycursor.fetchall()
+    for row in myresult:
+        print(row)
+    mycursor.close()
+    mydb.close()
